1dafd767d3ddb1e562b010f3dea3234bd7a3e39b
jbalboaU <juan.balboa@ug.uchile.cl>
2025-04-08 23:54:41 -0400

    entrega-de-tarea-1

---
+++ portada.js
@@ -0,0 +1,11 @@
+document.getElementById("add").addEventListener("click", function(){
+    window.location.href = "add.html"
+});
+
+document.getElementById("view").addEventListener("click", function(){
+    window.location.href = "view.html"
+});
+
+document.getElementById("stats").addEventListener("click", function(){
+    window.location.href = "stats.html"
+});
